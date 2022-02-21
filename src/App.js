6feda599b76db8da3692f822447aecb5dd114a61
ZakBht18 <zikobht2002@gmail.com>
2022-02-21 21:12:42 +0100

    aaa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home__container'>
+        <img
+          src='https://m.media-amazon.com/images/I/81UwfObBWFL.jpg'
+          className='home__image'
+          alt=''
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
